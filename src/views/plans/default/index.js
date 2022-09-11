a7f9e105572c3bfb0cba0429098c05a186c70e8b
Dinh Hoang Nam <dinhhoangnam998@gmail.com>
2022-09-11 11:38:36 +0700

    :pencil2: add events to calendar

--- src/views/plans/default/index.js
+++ src/views/plans/default/index.js
@@ -2,5 +2,16 @@ import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 export default function Plan() {
-    return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={[
+                { title: 'ABC Club Meeting', date: '2022-09-02' },
+                { title: 'Hackathon', date: '2022-09-10' },
+                { title: 'SheCodes', date: '2022-09-11' },
+                { title: 'Computer Vision Deadline', date: '2022-09-14' }
+            ]}
+        />
+    );
 }
